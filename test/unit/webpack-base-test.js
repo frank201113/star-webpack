da5a7391f6767e1ed7d40aed07213352cefb1a10
frank201113 <1831328181@qq.com>
2021-03-14 17:15:10 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('builder-webpack test case', () => {
   const baseConfig = require('../../lib/webpack.base.js')
   console.log(baseConfig);
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, 'D:/work/code/demo/unit-demo/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, 'D:/work/code/demo/unit-demo/builder-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
